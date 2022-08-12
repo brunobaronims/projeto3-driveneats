0cff2a74dadd1a7ded200cd56d6340a007602533
Bruno Baroni <brunobaronims@gmail.com>
2022-08-12 15:34:56 -0300

    finished static layout

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -2,14 +2,14 @@ function createLists() {
   const names = ['Frango Yin Yang', 'Coquinha gelada', 'Pudim'];
   const descriptions = ['Um pouco de batata, um pouco de salada', 'Lata 350ml', 'Apenas pudim'];
   const prices = ['R$ 14,90', 'R$ 4,90', 'R$ 7,90'];
-  const images = ['../public/images/frango_yin_yang 1.jpg', '../public/images/coquinha_gelada 1.jpg', '../public/images/pudim 1.jpg'];
+  const images = ['../../public/images/frango_yin_yang 1.jpg', '../../public/images/coquinha_gelada 1.jpg', '../../public/images/pudim 1.jpg'];
   const info = [images, names, descriptions, prices];
   const listTypes = ['.dishes', '.drinks', '.desserts'];
   const tag = ['img', 'h3', 'p', 'span'];
-  listTypes.map((Type, list) => {
+  listTypes.forEach((Type, list) => {
     let ul = document.querySelector([Type])
     let li = document.createElement('li');
-    info.map((Info, line) => {
+    info.forEach((Info, line) => {
       if (Info === images){
         li.appendChild(Object.assign(document.createElement(tag[line]), {src: Info[list]}));
       }
@@ -17,7 +17,7 @@ function createLists() {
         li.appendChild(Object.assign(document.createElement(tag[line]), {innerHTML: Info[list]}));
       }
     })
-    info.map((Index) => {
+    info.forEach((Index) => {
       const clone = li.cloneNode(true);
       ul.appendChild(clone);
     })
