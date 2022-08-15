f05c632b7ab769a00905bb600d9046bd7a79c510
Bruno Baroni <brunobaronims@gmail.com>
2022-08-14 23:35:23 -0300

    order button reactivity, updated item highlighting

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -1,29 +1,88 @@
-function createLists() {
-  const names = ['Frango Yin Yang', 'Coquinha gelada', 'Pudim'];
-  const descriptions = ['Um pouco de batata, um pouco de salada', 'Lata 350ml', 'Apenas pudim'];
-  const prices = ['R$ 14,90', 'R$ 4,90', 'R$ 7,90'];
-  const images = ['../../public/images/frango_yin_yang 1.jpg', '../../public/images/coquinha_gelada 1.jpg', '../../public/images/pudim 1.jpg'];
-  const info = [images, names, descriptions, prices];
-  const listTypes = ['.dishes', '.drinks', '.desserts'];
-  const tag = ['img', 'h3', 'p', 'span'];
-  listTypes.forEach((Type, list) => {
-    let ul = document.querySelector([Type])
-    let li = document.createElement('li');
-    info.forEach((Info, line) => {
-      if (Info === images){
-        li.appendChild(Object.assign(document.createElement(tag[line]), {src: Info[list]}));
-      }
-      else {
-        li.appendChild(Object.assign(document.createElement(tag[line]), {innerHTML: Info[list]}));
-      }
-    })
-    info.forEach((Index) => {
-      const clone = li.cloneNode(true);
-      ul.appendChild(clone);
-    })
-  })
+function getHighlightedElement(activeList) {
+  activeElement = Array.from(activeList)[0];
+  return activeElement;
 }
 
-window.addEventListener('DOMContentLoaded', ()=> {
-  createLists();
-});
+function getClickedItem(event) {
+  if (event.target.onclick === null) {
+    let li = event.target.parentElement;
+    return li;
+  }
+  else {
+    let li = event.target;
+    return li;
+  }
+}
+
+function highlightDish(event) {
+    let activeDish = document.getElementsByClassName('dish-active');
+    let activeDrink = document.getElementsByClassName('drink-active');
+    let activeDessert = document.getElementsByClassName('dessert-active');
+    clickedItem = getClickedItem(event);
+    if (activeDish.length > 0) {
+      activeElement = getHighlightedElement(activeDish);
+      if (!(clickedItem === activeElement)) {
+        activeElement.classList.toggle('dish-active');
+        clickedItem.classList.toggle('dish-active');
+      }
+      else {
+        clickedItem.classList.toggle('dish-active');
+      }
+    }
+    else {
+      clickedItem.classList.toggle('dish-active');
+    }
+
+
+    let activeDishes = activeDish.length;
+    let activeDrinks = document.getElementsByClassName('drink-active').length;
+    let activeDesserts = document.getElementsByClassName('dessert-active').length;
+    if (activeDishes + activeDrinks + activeDesserts === 3) {
+      document.querySelector('footer.fixed-footer button').classList.toggle('active-order-button');
+    //https://wa.me/?text=urldamensagempronta
+    }
+}
+
+function highlightDrink(event) {
+    let activeList = document.getElementsByClassName('drink-active');
+      Array.from(activeList).forEach(Drink =>
+        Drink.classList.toggle('drink-active')  
+    );
+    if (event.target.onclick === null) {
+      let li = event.target.parentElement;
+      li.classList.toggle('drink-active');
+    }
+    else {
+      let li = event.target;
+      li.classList.toggle('drink-active');
+    }
+
+    let activeDrinks = activeList.length;
+    let activeDishes = document.getElementsByClassName('dish-active').length;
+    let activeDesserts = document.getElementsByClassName('dessert-active').length;
+    if (activeDishes + activeDrinks + activeDesserts === 3) {
+      document.querySelector('footer.fixed-footer button').classList.toggle('active-order-button');
+    }
+}
+
+function highlightDessert(event) {
+    let activeList = document.getElementsByClassName('dessert-active');
+      Array.from(activeList).forEach(Dessert =>
+        Dessert.classList.toggle('dessert-active')  
+    );
+    if (event.target.onclick === null) {
+      let li = event.target.parentElement;
+      li.classList.toggle('dessert-active');
+    }
+    else {
+      let li = event.target;
+      li.classList.toggle('dessert-active');
+    }
+
+    let activeDesserts = activeList.length;
+    let activeDishes = document.getElementsByClassName('dish-active').length;
+    let activeDrinks = document.getElementsByClassName('drink-active').length;
+    if (activeDishes + activeDrinks + activeDesserts === 3) {
+      document.querySelector('footer.fixed-footer a').classList.toggle('active-order-button');
+    }
+}
