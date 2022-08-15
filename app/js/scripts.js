a5e503ba1d77828d1ab231bb6d88ef29389c6314
Bruno Baroni <brunobaronims@gmail.com>
2022-08-15 10:58:51 -0300

    finished main functionalities

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -11,13 +11,13 @@ function getClickedItem(event) {
 
 function getHighlightedItems() {
   let highlightedItems = [document.getElementsByClassName('dishes-active'),
-   document.getElementsByClassName('drinks-active'), document.getElementsByClassName('desserts-active')];
+  document.getElementsByClassName('drinks-active'), document.getElementsByClassName('desserts-active')];
   return highlightedItems;
 }
 
-function highlightItem(event) {
-  clickedItem = getClickedItem(event);
-  clickedItemType = clickedItem.parentElement.classList[0];
+function highlightItem(event) {  
+  let clickedItem = getClickedItem(event);
+  let clickedItemType = clickedItem.parentElement.classList[0];
   clickedItem.classList.toggle(clickedItemType + '-active');
   highlightedItems = getHighlightedItems();
   if ((highlightedItems[0].length || highlightedItems[1].length || highlightedItems[2].length) > 0) {
@@ -36,15 +36,31 @@ function highlightItem(event) {
   buttonElement = document.querySelector('footer.fixed-footer a');
   if ((highlightedItems[0].length + highlightedItems[1].length + highlightedItems[2].length) === 3) {
     buttonElement.classList.add('active-order-button');
-    //https://wa.me/?text=urldamensagempronta
+
+    let dishInfo = {
+      name: Array.from(highlightedItems[0])[0].querySelector('h3').innerHTML,
+      price: Array.from(highlightedItems[0])[0].querySelector('h1').innerHTML
+    };
+    let drinkInfo = {
+      name: Array.from(highlightedItems[1])[0].querySelector('h3').innerHTML,
+      price: Array.from(highlightedItems[1])[0].querySelector('h1').innerHTML
+    };
+    let dessertInfo = {
+      name: Array.from(highlightedItems[2])[0].querySelector('h3').innerHTML,
+      price: Array.from(highlightedItems[2])[0].querySelector('h1').innerHTML
+    };
+
+    totalPrice = (parseFloat(dishInfo.price) + parseFloat(drinkInfo.price)
+      + parseFloat(dessertInfo.price)).toFixed(2);
+
+    const orderText = `Olá, gostaria de fazer o pedido: - Prato: ${dishInfo.name} - Bebida: ${drinkInfo.name} - Sobremesa: ${dessertInfo.name} Total : R$ ${totalPrice}`;
+    const orderURL = encodeURIComponent(orderText);
+
+    buttonElement.setAttribute("href", `https://wa.me/?text=${orderURL}`);
+    buttonElement.setAttribute("style", "text-decoration: none");
   }
   else {
     buttonElement.classList.remove('active-order-button');
+    buttonElement.removeAttribute("href");
   }
-}
-
-function placeOrder() {
-  highlightedItems = getHighlightedItems();
-  dishName = highlightedItems[0].querySelector('h3').innerHTML;
-  console.log(dishName);
 }
